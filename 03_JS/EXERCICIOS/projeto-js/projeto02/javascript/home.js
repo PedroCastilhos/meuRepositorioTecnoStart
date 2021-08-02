a9f0decb8e2c9dac45b022389ee190b7758be9cd
PedroCastilhos <pedrocastilhos99@gmail.com>
2021-08-02 19:13:50 -0300

    Projeto-JS

---
+++ 03_JS/EXERCICIOS/projeto-js/projeto02/javascript/home.js
@@ -0,0 +1,63 @@
+/* ----------------------------------
+
+var money = document.querySelector("#receber");
+
+receber.addEventListener("click", function {
+
+  var receber = document.querySelector("#1");
+
+  var value = receber.value;
+
+  console.log(receber)
+});
+
+
+var demo = window.document.getElementById("demo")
+window.document.write("teste")
+
+document.getElementById("Saldo").
+
+var saldo
+
+var receber
+var depositar
+var pagar
+
+var cartfis
+var contasal
+var binter
+var picpay
+var emerg
+var invest
+
+if ( receber => )
+
+
+
+ var botaoSaldo = window.document.getElementById('Saldo')
+
+botaoSaldo.addEventListener('click', notificar)
+
+function notificar () {  
+ var inputSaldo = Number(window.document.getElementById('Saldo')).value;
+  var body = window.document.getElementsByTagName('body')[0]
+  
+  body.innerHTML += "<p> teste </p>";
+
+}
+
+
+ var inputSaldo = Nunber(window.prompt("Qual é o saldo?"))
+window.document.write(inputRadar);
+
+
+ var inputSaldo = window.document.getElementByIdnt("Saldo").innerText;
+window.document.write(inputSaldo)  
+
+var agora = new Date()
+var data = agora.getDate()
+
+var data = window.document.getElementById("date").innerText;
+window.document.write(pdate)
+
+*/
