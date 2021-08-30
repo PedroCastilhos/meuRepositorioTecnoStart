bfca40d7b7d96d395df8f81f6592945a46d3f90e
PedroCastilhos <qscdurant8>
2021-08-30 17:44:41 -0300

    CampoMinado

---
+++ 03_JS/EXERCICIOS/aula-25/index.js
@@ -0,0 +1,81 @@
+const tabuleiro = document.querySelector('#Tabuleiro');
+const linhas = tabuleiro.querySelectorAll('tr');
+
+
+linhas.forEach((linha, indiceL) => {
+
+    const colunas = linha.querySelectorAll('td');
+
+
+    colunas.forEach((td, indiceC) => {
+
+        const campo = td.querySelectorAll('.campo')[0];
+
+        atribuirBombasAosCampos({
+            campo,
+            indiceL,
+            indiceC,
+        });
+
+        campo.addEventListener('click', () => {
+            if (campo.className.includes("bomba")) {
+                campo.className = campo.className.replace('bomba', 'explodido')
+                window.alert('perdeu o jogo :(')
+            }
+
+        })
+
+        campo.addEventListener('click', () => {
+            if (!campo.className.includes("aberto")) {
+                if (campo.className.includes("aberto")) {
+                    campo.className = "campo"
+                    campo.innerText = ""
+                } else {
+                    campo.className = "campo aberto"
+                    campo.innerText = ""
+                }
+            }
+
+        })        
+        
+
+        campo.addEventListener('contextmenu', () => {
+
+
+            if (!campo.className.includes("aberto")) {
+                if (campo.className.includes("marcado")) {
+                    campo.className = campo.className.replace(' marcado', '')
+                    campo.innerText = ""
+                } else {
+                    campo.className = "campo marcado"
+                    campo.innerText = "🚩"
+                }
+            }
+
+
+
+
+        })
+
+
+
+    })
+
+})
+
+
+function atribuirBombasAosCampos(elemento) {
+    Math.random();
+
+    if (Math.random() > 0.67) {
+        elemento.campo.innerText = '💣'
+        elemento.className = 'campo bomba'        
+    } else {
+        elemento.campo.innerText = ""
+        elemento.className = 'campo'
+
+    }
+
+}
+
+       
